e931a209366cdfaf2f1cf34659ac2bfe047be0ef
Fahim Al Islam <34773501+dev-fahim@users.noreply.github.com>
2020-12-17 23:07:05 +0600

    Update global.js

--- js/global.js
+++ js/global.js
@@ -98,7 +98,7 @@ $(function() {
         }
     });
 
-    var date = new Date("01 Jan 2020");
+    var date = new Date("01 Jan 2021");
     var now = new Date();
     var diff = (date.getTime()/1000) - (now.getTime()/1000);  
 
